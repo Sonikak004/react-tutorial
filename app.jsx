1c897aa83be6ca82a51b324104e4cdd90f92c24d
Sonika Kumar <sonikak004@gmail.com>
2023-07-27 08:05:47 +0100

    Add app and main.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import TodoList from './TodoList';
+import './app.css'; // Import the CSS file
+
+function App() {
+    return (
+        <div className="App container">
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
